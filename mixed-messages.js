51d009369300bc89e639b20d9176611ae251cd54
Edem Cudjoe <edem.cudjoe@amalitech.org>
2022-02-03 16:37:32 +0000

    add variables containing random selections from containers

--- mixed-messages.js
+++ mixed-messages.js
@@ -5,5 +5,12 @@ const randomMessage = () => {
     const container3 = ['feel', 'encounter', 'create', 'meet', 'see', 'get', 'take', 'rekindle', 'make', 'trust', ];
     const container4 = ['the universe', 'a connection', 'someone', 'an opportunity', 'a surprise', 'some good news', 'an old flame', 'a sign', 'some bad news', 'an adventure', 'a financial opportunity', 'soul mate'];
     
+    //selection of random pieces from containers and stored in varirables
+    let selection1 = container1[Math.floor(Math.random() * container1.length)];
+    let selection2 = container2[Math.floor(Math.random() * container2.length)];
+    let selection3 = container3[Math.floor(Math.random() * container3.length)];
+    let selection4 = container4[Math.floor(Math.random() * container4.length)];
+
+        
 }
 
