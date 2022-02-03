b5c9a403cc17b47f742d6d631f88bfc5c6ac170c
Edem Cudjoe <edem.cudjoe@amalitech.org>
2022-02-03 16:25:59 +0000

    add arrays containing message pieces

---
+++ mixed-messages.js
@@ -0,0 +1,9 @@
+const randomMessage = () => {
+    //message parts stored in arrays
+    const container1 = ['Aries', 'Taurus', 'Gemini', 'Cancer', 'Leo', 'Virgo', 'Libra', 'Scorpio', 'Sagittarius', 'Capricorn', 'Aquarius', 'Pisces'];
+    const container2 = ['are to', 'can', 'should', 'will', 'might', 'love to'];
+    const container3 = ['feel', 'encounter', 'create', 'meet', 'see', 'get', 'take', 'rekindle', 'make', 'trust', ];
+    const container4 = ['the universe', 'a connection', 'someone', 'an opportunity', 'a surprise', 'some good news', 'an old flame', 'a sign', 'some bad news', 'an adventure', 'a financial opportunity', 'soul mate'];
+    
+}
+
