0f9a1fe4dade75dae2c13bc618fe584b0fd85d6c
Edem Cudjoe <edem.cudjoe@amalitech.org>
2022-02-03 17:16:48 +0000

    add new message container and change return to console.log

--- mixed-messages.js
+++ mixed-messages.js
@@ -4,15 +4,17 @@ const randomMessage = () => {
     const container2 = ['are to', 'can', 'should', 'will', 'might', 'love to'];
     const container3 = ['find', 'encounter', 'create', 'meet', 'see', 'get', 'take', 'rekindle', 'make', 'trust', 'engage'];
     const container4 = ['the universe', 'a connection', 'someone', 'an opportunity', 'a surprise', 'some good news', 'an old flame', 'a sign', 'some bad news', 'an adventure', 'a financial opportunity', 'a soul mate'];
-    
+    const container5 = ['soon', 'today', 'tomorrow', 'immediately', 'in a week\'s time', 'in a fortnight', 'next month', 'next year'];
+
     //selection of random pieces from containers and stored in varirables
     let selection1 = container1[Math.floor(Math.random() * container1.length)];
     let selection2 = container2[Math.floor(Math.random() * container2.length)];
     let selection3 = container3[Math.floor(Math.random() * container3.length)];
     let selection4 = container4[Math.floor(Math.random() * container4.length)];
+    let selection5 = container5[Math.floor(Math.random() * container5.length)];
 
     //returns a message made up of random selections
-    return `${selection1} ${selection2} ${selection3} ${selection4}`;        
+    console.log(`${selection1} ${selection2} ${selection3} ${selection4} ${selection5}`);        
 }
 
-console.log(randomMessage());
+randomMessage();
